9eeb91e4af0c7310f5c6aabb33ac62051c663e78
waynezxcv <liuweiself@126.com>
2015-12-23 11:29:10 +0800

    cocoachinaSpider

--- cocoachina/items.py
+++ cocoachina/items.py
@@ -11,4 +11,5 @@ import scrapy
 class CocoachinaItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+	title = scrapy.Field()
+	url = scrapy.Field()
--- cocoachina/spiders/cocoachinaSpider.py
+++ cocoachina/spiders/cocoachinaSpider.py
@@ -4,6 +4,10 @@
 from cocoachina.items import CocoachinaItem
 import scrapy
 from scrapy.selector import Selector
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.contrib.linkextractors import LinkExtractor
+from scrapy.http import Request
 
 
 
@@ -11,17 +15,22 @@ class cocoachinaSpider(scrapy.Spider):
     name = "cocoachina"
     allowed_domains = ["cocoachina.com"]
     start_urls = [
-        "http://www.cocoachina.com/news/",
+        "http://www.cocoachina.com/news/index.php?num=2",
     ]
-
+    url = 'http://www.cocoachina.com/news/index.php?num='
+    page = 2
     def parse(self, response):
-
         for clearfixSelector in response.xpath('//li[@class="clearfix"]'):
             dateSelector = clearfixSelector.xpath('h6[@class="float-l"]/text()').extract();
             for date in dateSelector:
                print "---------------- %s -------------" % date
-
             for sel in clearfixSelector.xpath('ul/li/a'):
                 title = sel.xpath('text()').extract()[0]
                 url = sel.xpath('@href').extract()[0]
-                print title,url
+                item = CocoachinaItem()
+                item['title'] = title
+                item['url'] = url
+                print title,url                
+                # yield item
+            self.page = self.page + 1
+            yield Request(self.url + '%s' % self.page,callback = self.parse)
