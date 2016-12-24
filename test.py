f5b82211be826a4c7d57238396b52d2107e7bbc7
Wei Liu <waynezxcv@users.noreply.github.com>
2016-12-25 03:30:01 +0800

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import mysql.connector
 import json
 
 
-conn = mysql.connector.connect(host = 'rds5r1sifk46m17i665c.mysql.rds.aliyuncs.com',port = '3306',user='waynezxcv', password='shihoujian1', database='cocoachina', use_unicode=True)
+conn = mysql.connector.connect(host = 'rds5r1sifk46m17i665c.mysql.rds.aliyuncs.com',port = '3306',user='waynezxcv', password='123456', database='cocoachina', use_unicode=True)
 cursor = conn.cursor()
 cursor.execute('select * from t_article')
 values = cursor.fetchall()
